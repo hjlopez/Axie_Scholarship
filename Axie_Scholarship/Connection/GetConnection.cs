c233d4bcbef096b326e28a50c80ac8cc5b112da0
hjlopez <49752106+hjlopez@users.noreply.github.com>
2022-02-01 18:05:35 +0800

    Connection String

---
+++ Axie_Scholarship/Connection/GetConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axie_Scholarship.Connection
+{
+    public static class GetConnection
+    {
+        public static string Connection()
+        {
+            return ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        }
+    }
+}
